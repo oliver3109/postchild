983287c0def5185efe71251a6eec842974adc173
Oliver <810982048@qq.com>
2021-11-19 15:22:41 +0800

    fix(config): 缺失根目录

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,36 @@ import { NuxtConfig } from "@nuxt/types";
 const env = process.env.NODE_ENV;
 
 const config: NuxtConfig = {
+  head: {
+    titleTemplate: "%s",
+    title: "Postchild • 一个免费好看的API请求构建工具",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "keywords",
+        content:
+          "postchild, postman, api, request, tool, test tool, api test tool, similar postman, postman instead, 接口测试工具, API测试, 类似postman, postman代替, 好用的接口测试工具",
+      },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "模仿 Postman，搭建了一个免费好看的 API 请求构建工具，帮助您更高效的开发和测试。",
+      },
+      {
+        name: "X-UA-Compatible",
+        content: "IE=edge, chrome=1",
+      },
+      { name: "baidu-site-verification", content: "yHZaLQtn7p" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+
+  router: {
+    base: "/postchild/",
+  },
+
   buildDir: "dist/.nuxt",
   generate: { dir: "dist/renderer" },
   css: [
